bc41507e979cb926047162ea0a4d336e9f43eefe
onovich <hjn1110@163.com>
2024-02-29 18:26:50 +0800

    <feature> modify: ProtocolIDConst

--- Scripts_Protocol/Common/Const/ProtocolDConst.cs
+++ Scripts_Protocol/Common/Const/ProtocolDConst.cs
@@ -4,21 +4,24 @@ namespace Ping.Protocol {
 
         // Req
         public const byte REQID_JOINROOM = 101;
-        public const byte REQID_HEARTBEAT = 102;
+        public const byte REQID_LEAVEROOM = 102;
+        public const byte REQID_HEARTBEAT = 103;
         public const byte REQID_PADDLEMOVE = 104;
-        public const byte REQID_RECONNECT = 108;
+        public const byte REQID_RECONNECT = 105;
 
         // Res
         public const byte RESID_JOINROOM = 201;
-        public const byte RESID_CONNECT = 207;
-        public const byte RESID_RECONNECT = 208;
+        public const byte RESID_CONNECT = 202;
+        public const byte RESID_RECONNECT = 203;
+        public const byte RESID_PADDLESYNC = 204;
 
         // Broad
-        public const byte BROADID_EXITGAME = 001;
-        public const byte BROADID_STARTGAME = 002;
-        public const byte BROADID_HEARTBEAT = 202;
-        public const byte BROADID_PADDLEMOVE = 204;
-        public const byte BROADID_BALLMOVE = 205;
+        public const byte BROADID_GAMERESULT = 001;
+        public const byte BROADID_LEAVEROOM = 002;
+        public const byte BROADID_STARTGAME = 003;
+        public const byte BROADID_HEARTBEAT = 004;
+        public const byte BROADID_PADDLEMOVE = 005;
+        public const byte BROADID_BALLMOVE = 006;
 
     }
 
