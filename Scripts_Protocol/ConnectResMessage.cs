11222b9d6477fae8de41bd581a7d681461c0156f
onovich <hjn1110@163.com>
2024-03-02 00:17:26 +0800

    WIP: Login

--- Scripts_Protocol/ConnectResMessage.cs
+++ Scripts_Protocol/ConnectResMessage.cs
@@ -6,17 +6,20 @@ namespace Ping.Protocol {
     public struct ConnectResMessage : IMessage<ConnectResMessage> {
 
         public sbyte status; // 1 为成功, -1 为失败
+        public byte playerIndex;
 
         public void WriteTo(byte[] dst, ref int offset) {
             ByteWriter.Write<sbyte>(dst, status, ref offset);
+            ByteWriter.Write<byte>(dst, playerIndex, ref offset);
         }
 
         public void FromBytes(byte[] src, ref int offset) {
             status = ByteReader.Read<sbyte>(src, ref offset);
+            playerIndex = ByteReader.Read<byte>(src, ref offset);
         }
 
         public int GetEvaluatedSize(out bool isCertain) {
-            int count = ByteCounter.Count<sbyte>();
+            int count = ByteCounter.Count<sbyte>() + ByteCounter.Count<byte>();
             isCertain = true;
             return count;
         }
