ad94485bad234f59a382f8e2d0ed9c519e68fe3c
onovich <hjn1110@163.com>
2024-03-15 17:59:32 +0800

    <feature> modify: Local IP

--- Scripts_Runtime/Infra_Request/Common/Const/RequestConst.cs
+++ Scripts_Runtime/Infra_Request/Common/Const/RequestConst.cs
@@ -4,7 +4,7 @@ namespace Ping.Server.Requests {
 
     public static class RequestConst {
 
-        public static IPAddress LOCAL_IP = IPAddress.Loopback;
+        public static IPAddress LOCAL_IP = IPAddress.Any;
         public const int LOCAL_PORT = 8080;
 
     }
